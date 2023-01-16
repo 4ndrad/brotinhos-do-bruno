be63390ae5ab07e083112cfbfc7e4f4f80d97084
Natan Andrade de Lima <natanal@primeit.com.br>
2023-01-16 10:42:58 -0300

    [FRONT-0003] - criação da tela de consult (incompleta), apenas com o search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component'
+import {ConsultComponent} from './pages/consult/consult.component'
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'consult', component: ConsultComponent}
 ];
 
 @NgModule({
